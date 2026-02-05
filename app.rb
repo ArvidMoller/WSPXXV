0d0cfd207223e249caa7282866d6b393ff51e8dc
Möllinatorn3000 <arvid.moller@elev.ga.ntig.se>
2026-02-05 16:53:29 +0100

    Wrote code for the database and started writing slim and routes.

--- app.rb
+++ app.rb
@@ -3,3 +3,21 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
+
+def load_db(name)
+  db = SQLite3::Database.new("db/#{name}.db")
+  db.results_as_hash = true
+
+  return db
+end
+
+
+get("/index") do
+  slim(:index)
+end
+
+post("/rooms/search") do
+  db = load_db("databas")
+
+  room_search = params[:room_search]
+end
--- db/seeder.rb
+++ db/seeder.rb
@@ -15,27 +15,73 @@ def seed!(db)
 end
 
 def drop_tables(db)
-  db.execute('DROP TABLE IF EXISTS exempel')
+  db.execute('DROP TABLE IF EXISTS users')
+  db.execute('DROP TABLE IF EXISTS rooms')
+  db.execute('DROP TABLE IF EXISTS room_category')
+  db.execute('DROP TABLE IF EXISTS booking_category')
+  db.execute('DROP TABLE IF EXISTS user_room_rel')
+  db.execute('DROP TABLE IF EXISTS room_category_rel')
 end
 
+# FIXA KORREKTA RELATIONSTABELLER
+
 def create_tables(db)
-  db.execute('CREATE TABLE exempel (
+  db.execute('CREATE TABLE users (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               name TEXT NOT NULL, 
-              description TEXT,
-              state BOOLEAN)')
+              pwd_digest TEXT NOT NULL,
+              teacher BOOL)')
+
+  db.execute('CREATE TABLE rooms (
+              id INTEGER PRIMARY KEY AUTOINCREMENT, 
+              name TEXT NOT NULL)')
+
+  db.execute('CREATE TABLE room_category (
+              id INTEGER PRIMARY KEY AUTOINCREMENT, 
+              category TEXT NOT NULL)')
+
+  db.execute('CREATE TABLE booking_category (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              category TEXT NOT NULL)')
+
+  db.execute('CREATE TABLE user_room_rel (
+              u_id INTEGER NOT NULL, 
+              r_id INTEGER NOT NULL, 
+              reason TEXT, 
+              start_time TEXT NOT NULL, 
+              end_time TEXT NOT NULL, 
+              booking_category INTEGER,
+              PRIMARY KEY (u_id, r_id), 
+              FOREIGN KEY (u_id) REFERENCES users(id)
+                ON DELETE CASCADE,
+              FOREIGN KEY (r_id) REFERENCES rooms(id)
+                ON DELETE CASCADE,
+              FOREIGN KEY (booking_category) REFERENCES booking_category(id))')
+
+  db.execute('CREATE TABLE room_category_rel (
+              r_id INTEGER NOT NULL, 
+              c_id INTEGER NOT NULL, 
+              PRIMARY KEY (r_id, c_id), 
+              FOREIGN KEY (r_id) REFERENCES rooms(id) 
+                ON DELETE CASCADE, 
+              FOREIGN KEY (c_id) REFERENCES room_categories(id) 
+                ON DELETE CASCADE)')
 end
 
 def populate_tables(db)
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Köp mjölk", "3 liter mellanmjölk, eko",false)')
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Köp julgran", "En rödgran",false)')
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Pynta gran", "Glöm inte lamporna i granen och tomten",false)')
+  db.execute('INSERT INTO rooms (name) VALUES ("204")')
+  db.execute('INSERT INTO rooms (name) VALUES ("318")')
+  db.execute('INSERT INTO rooms (name) VALUES ("316")')
+  db.execute('INSERT INTO rooms (name) VALUES ("425")')
+
+  db.execute('INSERT INTO room_category (category) VALUES ("Klassrum")')
+  db.execute('INSERT INTO room_category (category) VALUES ("Kemisal")')
+  db.execute('INSERT INTO room_category (category) VALUES ("Grupprum")')
+
+  db.execute('INSERT INTO booking_category (category) VALUES ("Lektion")')
+  db.execute('INSERT INTO booking_category (category) VALUES ("Klubbverksamhet")')
+
 end
 
 
 seed!(db)
-
-
-
-
-
